ae7548e8e1f7c471feefdec5563e5afe66c05ddd
devlee77 <tjdcks0928@naver.com>
2020-09-25 14:27:31 +0900

    add create table funtion

--- MyDatabase/app/src/main/java/com/example/database/MainActivity.java
+++ MyDatabase/app/src/main/java/com/example/database/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
+    EditText editText2;
+
     TextView textView;
     SQLiteDatabase database;
 
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
         textView = (TextView) findViewById(R.id.textView);
 
         Button button = (Button) findViewById(R.id.button);
@@ -29,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String databaseName = editText.getText().toString();
                 openDatabase(databaseName);
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tableName = editText2.getText().toString();
+                createTable(tableName);
 
             }
         });
@@ -42,9 +56,20 @@ public class MainActivity extends AppCompatActivity {
         if (database != null) {
             println("데이터베이스 오픈됨.");
         }
+    }
 
 
+    public void createTable(String tableName) {
+        println("createTable() 호출됨");
 
+        if (database != null) {
+            String sql = "create table " + tableName + "(_id integer PRIMARY KEY autoincrement, name text, age integer, mobile text)";
+            database.execSQL(sql);
+
+            println("테이블 생성됨.");
+        } else {
+            println("먼저 데이터 베이스를 오픈하세요.");
+        }
     }
 
     public void println(String data) {
